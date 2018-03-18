c701992020db43ae4ecb35c2fee378fc1519ecd9
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-18 11:50:28 -0500

    Add user model

---
+++ src/app/user/model.ts
@@ -0,0 +1,67 @@
+import './mongoose';
+import * as mongoose from 'mongoose';
+
+export interface User {
+  username: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  role: Role;
+  salt: string;
+  password: string;
+}
+
+export enum Role {
+  'admin',
+  'teacher',
+  'student'
+}
+
+export interface UserDocument extends User, mongoose.Document {
+}
+
+let UserSchema: mongoose.Schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    matches: /^[a-zA-Z\d]([a-zA-Z\d]|[_-][a-zA-Z\d])+$/,
+    maxlength: 32,
+    unique: true
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 100
+  },
+  email: {
+    type: String,
+    required: true,
+    matches: /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: [
+      'admin',
+      'teacher',
+      'student'
+    ]
+  },
+  salt: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+export let AlbumModel: mongoose.Model<UserDocument> = mongoose.model<UserDocument>('User', UserSchema);
