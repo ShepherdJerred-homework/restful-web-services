d07d1db888840cab268732449a9b11ec421b9be1
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-19 14:30:49 -0500

    Store user in res.locals.user, add getUserFromParameter function. Add deleteUser implementation

--- src/app/user/controller.ts
+++ src/app/user/controller.ts
@@ -6,6 +6,24 @@ import * as mongoose from 'mongoose';
 
 let pbkdf2 = util.promisify(crypto.pbkdf2);
 
+export async function getUserFromParameter (req: express.Request, res: express.Response, next: express.NextFunction) {
+  let user: model.User | null;
+
+  try {
+    // TODO a valid username may be interpreted as a object id
+    if (mongoose.Types.ObjectId.isValid(req.params.userid)) {
+      user = await model.UserModel.findOne({'_id': req.params.userid});
+    } else {
+      user = await model.UserModel.findOne({'username': req.params.userid});
+    }
+    res.locals.user = user;
+    next();
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+}
+
 // todo must be teacher or admin
 export async function getUsers (req: express.Request, res: express.Response, next: express.NextFunction) {
   try {
@@ -19,21 +37,7 @@ export async function getUsers (req: express.Request, res: express.Response, nex
 
 // todo must be teacher or admin
 export async function getUser (req: express.Request, res: express.Response, next: express.NextFunction) {
-  try {
-    let user: model.User | null;
-
-    // TODO a valid username may be interpreted as a object id
-    if (mongoose.Types.ObjectId.isValid(req.params.userid)) {
-      user = await model.UserModel.findOne({ '_id': req.params.userid });
-    } else {
-      user = await model.UserModel.findOne({ 'username': req.params.userid });
-    }
-
-    res.json(user);
-  } catch (err) {
-    res.status(500);
-    res.send(err);
-  }
+  res.json(res.locals.user);
 }
 
 // todo must be admin
@@ -64,9 +68,17 @@ export async function addUser (req: express.Request, res: express.Response, next
 
 // todo must be admin
 export async function updateUser (req: express.Request, res: express.Response, next: express.NextFunction) {
+
 }
 
 // todo must be admin
 export async function deleteUser (req: express.Request, res: express.Response, next: express.NextFunction) {
-
+  let user: model.UserDocument = res.locals.user;
+  try {
+    user = await user.remove();
+    res.json(user);
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
 }
