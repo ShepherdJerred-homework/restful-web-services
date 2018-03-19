c78830b8a058d59237596a3712702b8b26e61193
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-19 12:50:59 -0500

    Parse json, mount user router

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
+import * as bodyparser from 'body-parser';
+import * as userRoutes from './user/routes';
 
 export const app: express.Express = express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '../../../static'));
+app.use(bodyparser.json());
+
+app.use('/api/users', userRoutes.router);
