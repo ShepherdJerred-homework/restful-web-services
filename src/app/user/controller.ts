91eac6cd083c4c08e8c7ea3e8bedf639918c4cc4
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-19 14:24:23 -0500

    Check if userid param is an object id when getting user

--- src/app/user/controller.ts
+++ src/app/user/controller.ts
@@ -2,36 +2,41 @@ import * as express from 'express';
 import * as model from './model';
 import * as util from 'util';
 import * as crypto from 'crypto';
+import * as mongoose from 'mongoose';
 
 let pbkdf2 = util.promisify(crypto.pbkdf2);
 
+// todo must be teacher or admin
 export async function getUsers (req: express.Request, res: express.Response, next: express.NextFunction) {
   try {
     let users: model.User[] = await model.UserModel.find();
-    res.send(users);
+    res.json(users);
   } catch (err) {
     res.status(500);
     res.send(err);
   }
 }
 
+// todo must be teacher or admin
 export async function getUser (req: express.Request, res: express.Response, next: express.NextFunction) {
   try {
     let user: model.User | null;
-    if (isNaN(req.params.userid)) {
-      user = await model.UserModel.findOne({ 'username': req.params.userid });
-    } else {
+
+    // TODO a valid username may be interpreted as a object id
+    if (mongoose.Types.ObjectId.isValid(req.params.userid)) {
       user = await model.UserModel.findOne({ '_id': req.params.userid });
+    } else {
+      user = await model.UserModel.findOne({ 'username': req.params.userid });
     }
-    // TODO check if userid is id or username
-    // TODO don't send salt or password
-    res.send(user);
+
+    res.json(user);
   } catch (err) {
     res.status(500);
     res.send(err);
   }
 }
 
+// todo must be admin
 export async function addUser (req: express.Request, res: express.Response, next: express.NextFunction) {
   try {
     let salt = crypto.randomBytes(64);
@@ -50,15 +55,18 @@ export async function addUser (req: express.Request, res: express.Response, next
     });
 
     user = await user.save();
-    res.send(user);
+    res.json(user);
   } catch (err) {
     res.status(500);
     res.send(err);
   }
 }
 
+// todo must be admin
 export async function updateUser (req: express.Request, res: express.Response, next: express.NextFunction) {
 }
 
+// todo must be admin
 export async function deleteUser (req: express.Request, res: express.Response, next: express.NextFunction) {
+
 }
