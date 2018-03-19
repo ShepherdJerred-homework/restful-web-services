bd4149f651614a28644724c1ce8b3ec1b6994e80
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-19 14:29:51 -0500

    Call getUserFromParameter as middleware

--- src/app/user/routes.ts
+++ src/app/user/routes.ts
@@ -5,10 +5,10 @@ export const router: express.Router = express.Router();
 
 router.get('/', controller.getUsers);
 
-router.get('/:userid', controller.getUser);
+router.get('/:userid', controller.getUserFromParameter, controller.getUser);
 
 router.post('/', controller.addUser);
 
-router.put('/:userid', controller.updateUser);
+router.put('/:userid', controller.getUserFromParameter, controller.updateUser);
 
-router.delete('/:userid', controller.deleteUser);
+router.delete('/:userid', controller.getUserFromParameter, controller.deleteUser);
