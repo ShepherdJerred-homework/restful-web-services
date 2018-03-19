cb0a53998e16ce8d535d98e3e81cbca3adde3bc9
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-18 21:04:50 -0500

    Add code for filtering users by id, start on code for adding users

--- src/app/user/controller.ts
+++ src/app/user/controller.ts
@@ -1,27 +1,44 @@
 import * as express from 'express';
 import * as model from './model';
 
-export function getUsers (req: express.Request, res: express.Response, next: express.NextFunction) {
-  model.UserModel.find().then((users: model.User[]) => {
+export async function getUsers (req: express.Request, res: express.Response, next: express.NextFunction) {
+  try {
+    let users: model.User[] = await model.UserModel.find();
     res.send(users);
-  }).catch((err: any) => {
-    console.log(err);
-    res.sendStatus(500);
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+}
+
+export async function getUser (req: express.Request, res: express.Response, next: express.NextFunction) {
+  try {
+    // TODO check if userid is id or username
+    let user: model.User | null = await model.UserModel.findOne({ '_id': req.params.userid });
+    res.send(user);
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
+}
+
+export async function addUser (req: express.Request, res: express.Response, next: express.NextFunction) {
+  let user: model.UserModel = new model.UserModel({
+    username: req.body.username,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email
   });
+  try {
+    await user.save();
+  } catch (err) {
+    res.status(500);
+    res.send(err);
+  }
 }
 
-export function getUser (req: express.Request, res: express.Response, next: express.NextFunction) {
-  
+export async function updateUser (req: express.Request, res: express.Response, next: express.NextFunction) {
 }
 
-export function addUser (req: express.Request, res: express.Response, next: express.NextFunction) {
-
-}
-
-export function updateUser (req: express.Request, res: express.Response, next: express.NextFunction) {
-
-}
-
-export function deleteUser (req: express.Request, res: express.Response, next: express.NextFunction) {
-
+export async function deleteUser (req: express.Request, res: express.Response, next: express.NextFunction) {
 }
