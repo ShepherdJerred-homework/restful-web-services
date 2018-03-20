da0bdc60c0b32afdd9eea0216ba0cd59db209dd2
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-20 17:56:59 -0500

    Mount class router

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as morgan from 'morgan';
 import * as bodyparser from 'body-parser';
 import * as userRoutes from './user/routes';
+import * as classRoutes from './class/routes';
 
 export const app: express.Express = express();
 
@@ -10,3 +11,5 @@ app.use(express.static(__dirname + '../../../static'));
 app.use(bodyparser.json());
 
 app.use('/api/users', userRoutes.router);
+
+app.use('/api/classes', classRoutes.router);
