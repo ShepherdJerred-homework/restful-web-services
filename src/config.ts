5a2c6718cec0760b3fb3cdfe700216c4cedfd30b
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-03-20 18:42:38 -0500

    Add debug option

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
 export let serverPort: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
-export let mongodbUrl: string =  'mongodb://localhost:32768/project3';
+export let mongodbUrl: string = 'mongodb://localhost:32768/project3';
+
+export let debug: boolean = true;
